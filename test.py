b3d1f1f230e7ad1be89cb079c2209e5cfddbbc3f
Laura Dobie <moving.toyshop@gmail.com>
2020-11-09 22:01:05 +0000

    Chooses podcast from selection and adds it to picked podcasts file. Still to work out how to remove it from the podcast selection file

--- test.py
+++ test.py
@@ -3,27 +3,55 @@
 #Remove the podcast from the selection
 #Pick a podcast from the selection at random
 #Add it to the picked podcasts 
-
+import csv
 from random import choice
 
 podcast_selection = []
 picked_podcasts = []
 
-with open("podcast_selection.txt", 'r') as file:
-    for line in file:
-        line = line.strip("\n")
-        podcast_selection.append(line)
+with open('podcast_selection.csv') as csvfile:
+    podcasts = csv.reader(csvfile, delimiter=',')
+    for podcast in podcasts:
+        podcast_selection.append(podcast)
 
-with open("picked_podcasts.txt", 'r') as file:
-    for line in file:
-        line = line.strip("\n")
-        picked_podcasts.append(line)
+print("Podcast selection")
+for podcast in podcast_selection:
+    print(podcast)
+print(" ")
 
-# chosen_podcast = choice(podcast_selection)
-# print(chosen_podcast)
 
-# picked_podcasts.append(chosen_podcast)
-# podcast_selection.remove(chosen_podcast)
+chosen_podcast = choice(podcast_selection)
+print("Today's random pick is:")
+print(chosen_podcast)
+print(" ")
 
-print(podcast_selection)
-print(picked_podcasts)
+podcast_selection.remove(chosen_podcast)
+
+# chosen_podcast_index = podcast_selection.index(chosen_podcast)
+# print(chosen_podcast_index)
+
+with open('picked_podcasts.csv', mode="a") as csvfile:
+    chosen = csv.writer(csvfile, delimiter=',')
+    chosen.writerow(chosen_podcast)
+
+with open('picked_podcasts.csv') as csvfile:
+    chosen = csv.reader(csvfile, delimiter=',')
+    for podcast in chosen:
+        picked_podcasts.append(podcast)
+
+# with open('podcast_selection.csv', mode="a") as csvfile:
+#     writer = csv.writer(csvfile, delimiter=',')
+#     writer.writerows(podcast_selection)
+
+# with open('podcast_selection.csv', mode="a") as csvfile:
+#     writer = csv.writer(csvfile, delimiter=',')
+#     podcasts = csv.reader(csvfile, delimiter=',')
+#     for podcast in podcasts:
+#         if podcast == chosen_podcast:
+#             writer.writerow(chosen_podcast)
+
+print("Picked podcasts so far:")
+for podcast in picked_podcasts:
+    print(podcast)
+print(" ")
+
