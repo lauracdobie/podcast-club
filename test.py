e86bcd28cab2e6cc92e6922a1e10a2e9e5177eaa
Laura Dobie <moving.toyshop@gmail.com>
2020-11-10 14:42:10 +0000

    Code now works with txt files - ditched the CSV files

--- test.py
+++ test.py
@@ -3,18 +3,41 @@
 #Remove the podcast from the selection
 #Pick a podcast from the selection at random
 #Add it to the picked podcasts 
-import csv
+# import csv
 from random import choice
 
+# podcast_selection = []
+
+
+# with open('podcast_selection.csv') as csvfile:
+#     podcasts = csv.reader(csvfile, delimiter=',')
+#     for podcast in podcasts:
+#         podcast_selection.append(podcast)
+
+my_podcast_file = open("podcast_selection.txt", "r")
+
+podcast_list = my_podcast_file.readlines()
+my_podcast_file.close()
+
 podcast_selection = []
+for podcast in podcast_list: 
+	podcast_selection.append(podcast.replace("\n", ""))
+
+# print("Podcast selection")
+# for podcast in podcast_selection:
+#     print(podcast)
+# print(" ")
+
+my_podcast_file_2 = open("picked_podcasts.txt", "r")
+
+podcast_list_2 = my_podcast_file_2.readlines()
+my_podcast_file_2.close()
+
 picked_podcasts = []
+for podcast in podcast_list_2: 
+	picked_podcasts.append(podcast.replace("\n", ""))
 
-with open('podcast_selection.csv') as csvfile:
-    podcasts = csv.reader(csvfile, delimiter=',')
-    for podcast in podcasts:
-        podcast_selection.append(podcast)
-
-print("Podcast selection")
+print("Podcast selection:")
 for podcast in podcast_selection:
     print(podcast)
 print(" ")
@@ -26,18 +49,35 @@ print(chosen_podcast)
 print(" ")
 
 podcast_selection.remove(chosen_podcast)
+picked_podcasts.append(chosen_podcast)
+
+rewrite_data = open("podcast_selection.txt", "w+")
+
+for podcast in podcast_selection:
+	rewrite_data.write(podcast+"\n")
+
+rewrite_data.close()
+
+rewrite_data_2 = open("picked_podcasts.txt", "w+")
+
+for podcast in picked_podcasts:
+	rewrite_data_2.write(podcast+"\n")
+
+rewrite_data_2.close()
 
 # chosen_podcast_index = podcast_selection.index(chosen_podcast)
 # print(chosen_podcast_index)
 
-with open('picked_podcasts.csv', mode="a") as csvfile:
-    chosen = csv.writer(csvfile, delimiter=',')
-    chosen.writerow(chosen_podcast)
 
-with open('picked_podcasts.csv') as csvfile:
-    chosen = csv.reader(csvfile, delimiter=',')
-    for podcast in chosen:
-        picked_podcasts.append(podcast)
+
+# with open('picked_podcasts.csv', mode="a") as csvfile:
+#     chosen = csv.writer(csvfile, delimiter=',')
+#     chosen.writerow(chosen_podcast)
+
+# with open('picked_podcasts.csv') as csvfile:
+#     chosen = csv.reader(csvfile, delimiter=',')
+#     for podcast in chosen:
+#         picked_podcasts.append(podcast)
 
 # with open('podcast_selection.csv', mode="a") as csvfile:
 #     writer = csv.writer(csvfile, delimiter=',')
